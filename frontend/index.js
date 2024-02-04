ea9175baf305c826a6f4a9cccd25ce2022ba34e9
NagendraVarma1 <nagendravarmal512@gmail.com>
2024-02-04 22:19:09 +0530

    small changes

--- frontend/index.js
+++ frontend/index.js
@@ -1,41 +1,64 @@
-var form = document.getElementById('schedule-form')
+var form = document.getElementById("schedule-form");
 let time;
 
-function addNewMeeting(event){
-    event.preventDefault();
-    console.log('testing')
-    form.style.visibility = 'hidden'
+function testing(t) {
+  time = t;
+  form.style.visibility = "visible";
+}
 
-    const name = document.getElementById('name').value;
-    const mail = document.getElementById('mail').value;
+function addNewMeeting(event) {
+  event.preventDefault();
+  console.log("testing");
+  form.style.visibility = "hidden";
 
-    const meetingDetials = {
-        name, 
-        mail
-    }
+  const name = document.getElementById("name").value;
+  const mail = document.getElementById("mail").value;
 
-    
-    showMeetings(meetingDetials)
+  const meetingDetials = {
+    name,
+    mail,
+    time
+  };
 
-    document.getElementById('name').value = '';
-    document.getElementById('mail').value = '';
+  axios.post('https://crudcrud.com/api/d0d5d5363ca142dabd87f55a1ce06a58/meeting', meetingDetials)
+  .then((res) => {
+    showMeetings(res.data);
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+  
+
+  document.getElementById("name").value = "";
+  document.getElementById("mail").value = "";
 }
 
 function showMeetings(meeting) {
-    let div = document.getElementById('scheduledMeetings');
+  let div = document.getElementById("scheduledMeetings");
 
-    let card = document.createElement('div');
-    let person = document.createElement('h3')
-    let link = document.createElement('p');
+  let card = document.createElement("div");
+  card.className = "card";
+  let person = document.createElement("h3");
+  let link = document.createElement("p");
 
-    person.textContent = `Hi ${meeting.name}`
-    link.textContent = `Please join the meeting at ${time}`
+  person.textContent = `Hi ${meeting.name}`;
+  link.textContent = `Your meeting is scheduled at ${meeting.time}PM. Don't forget to join`;
 
-    card.appendChild(link);
-    div.append(card)
+  card.appendChild(person);
+  card.appendChild(link);
+  div.append(card);
 }
 
-function testing(t){
-    time = t
-    form.style.visibility = 'visible'
+function getAllMeetings() {
+    axios.get('https://crudcrud.com/api/d0d5d5363ca142dabd87f55a1ce06a58/meeting')
+    .then((res) => {
+        for(let i=0; i<res.data.length; i++){
+            showMeetings(res.data[i])
+        }
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 }
+getAllMeetings();
